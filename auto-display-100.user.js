f23d0e1d36b0fa2c05ab7d4c679ad602d8ae5f8c
Fikri <almunawarfikri@gmail.com>
2026-03-23 23:12:36 +0700

    Update auto-display-100.user.js

--- auto-display-100.user.js
+++ auto-display-100.user.js
@@ -5,6 +5,7 @@
 // @description  Set show entries jadi 100 otomatis
 // @match        http://192.168.3.16/smartplus/erm_ranap*
 // @match        http://192.168.3.16/smartplus/nurse_station/eranap*
+// @match        http://103.83.178.90:38/smartplus/erm_ranap
 // @updateURL    https://raw.githubusercontent.com/almunawarfikri/smartplus-tools/main/auto-display-100.user.js
 // @downloadURL  https://raw.githubusercontent.com/almunawarfikri/smartplus-tools/main/auto-display-100.user.js
 // @grant        none
